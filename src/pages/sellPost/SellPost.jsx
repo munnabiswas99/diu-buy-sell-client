be999b3ae3b1ba26fe739750a75208f8e950753b
munnabiswas99 <biswas15-5261@diu.edu.bd>
2026-05-07 20:14:38 +0600

    Firebase Auth Added

--- src/pages/sellPost/SellPost.jsx
+++ src/pages/sellPost/SellPost.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
 
 const SellPost = () => {
   const [loading, setLoading] = useState(false);
